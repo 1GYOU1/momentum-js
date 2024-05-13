acc1b6b0be056e11589930953bf4c5884433e16c
1GYOU1 <90018379+1GYOU1@users.noreply.github.com>
2024-05-13 22:00:43 +0900

    2024-05-13 update

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -1,9 +1,7 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const bgImage = document.querySelector(".wrap");
+
+const images = ["bg_img_1.jpg", "bg_img_2.jpg", "bg_img_3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+bgImage.style.backgroundImage = `url(img/${chosenImage})`;
